fda4719689296bd8d0d40640b1c20daa77596335
Edson Zucchi <edsonluiz.zuchi@gmail.com>
2024-03-09 10:37:41 -0300

    routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 import NotFound from "./pages/NotFound"
+import Login from "./pages/Login"
 import Home from "./pages/Home"
 
 
@@ -8,12 +9,27 @@ function App() {
   return (
     <BrowserRouter basename='/web'>
         <Routes>
-          <Route path={'/'} element={<Home/>}></Route>
-    
+          <Route path={'/login'} element={page(<Login/>)}></Route>
+          <Route path='/' element={page(<Home/>)}></Route>
           <Route path={'*'} element={<NotFound/>}></Route>
         </Routes>
     </BrowserRouter>
   );
+}
+
+function page(element){
+  if (element == <Login/>){
+    return <Login/>
+  }
+  if (loggedIn()) {
+    return element;
+  } else{
+    return <Navigate to={"/login"}></Navigate>
+  }
+}
+
+function loggedIn(){
+  return true;
 }
 
 export default App;
